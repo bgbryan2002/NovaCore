3e69c4672e47e64e8a4c18c4aabe690c77c12c54
bgbry <bgbryan@aggies.ncat.edu>
2025-04-06 00:07:49 -0400

    Remove unused files to clean up repository

--- run_analyzer.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-python meeting_analyzer.py "Senior Project Demo.mp3" --openai-key "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" --email-config email_config.json
-pause 
